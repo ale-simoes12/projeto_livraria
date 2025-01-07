ebaa67880ea0b1959238780ee3ef404af8a1775a
ale-simoes12 <alessandro.medrano321@gmail.com>
2025-01-07 15:28:35 -0300

    adicionando estados

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,5 +1,8 @@
 import Input from '../Input'
 import styled from 'styled-components'
+import { useState } from 'react'
+import { livros } from './dadosPesquisa'
+
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -25,12 +28,22 @@ const Subtitulo = styled.h3`
 
 
 function Pesquisa() {
+    const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
+
+    console.log(livrosPesquisados)
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input
-                placeholder="Escreva sua próxima leitura" />
+                placeholder="Escreva sua próxima leitura"
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado) )
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
+            />
         </PesquisaContainer>
     )
 }
